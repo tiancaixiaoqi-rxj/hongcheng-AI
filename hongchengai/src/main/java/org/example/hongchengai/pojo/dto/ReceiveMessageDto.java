ecf78ebd9a8440bb83440c0091061079292e28ed
5J1Na1 <1664988589@qq.com>
2024-09-23 17:34:51 +0800

    update

--- hongchengai/src/main/java/org/example/hongchengai/pojo/dto/ReceiveMessageDto.java
+++ hongchengai/src/main/java/org/example/hongchengai/pojo/dto/ReceiveMessageDto.java
@@ -1,12 +1,16 @@
 package org.example.hongchengai.pojo.dto;
 
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Arrays;
+
 /**
  * 用于接收前段传来的用户数据
  */
 public class ReceiveMessageDto {
     private String message;
-    private String file;
+    private MultipartFile files;
 
     public String getMessage() {
         return message;
@@ -16,19 +20,19 @@ public class ReceiveMessageDto {
         this.message = message;
     }
 
-    public String getFile() {
-        return file;
+    public MultipartFile getFiles() {
+        return files;
     }
 
-    public void setFile(String file) {
-        this.file = file;
+    public void setFiles(MultipartFile files) {
+        this.files = files;
     }
 
     @Override
     public String toString() {
         return "ReceiveMessageDto{" +
                 "message='" + message + '\'' +
-                ", file='" + file + '\'' +
+                ", files=" + files +
                 '}';
     }
 }
